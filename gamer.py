0bdb3466974b4e4d57596327001599db5952da95
Juanse <jtaborda579@soyudemedellin.edu.co>
2024-09-18 07:48:00 -0500

    Nueva version del juego

--- gamer.py
+++ gamer.py
@@ -3,16 +3,25 @@ import random
 nume_ran = random.randint(1, 100)
 playing = True
 
-computador = random(1, 100)
 
 while playing:
     guess = int(input("ingresa un numero:"))
     if guess == nume_ran:
         playing = False
-        print(f"el numero{guess} es el correcto")
+        print("felizidades eres el ganador")
+        break
     elif guess > nume_ran:
         print(f"el numero {guess} es mayor")
     elif guess < nume_ran:
         print(f"el numero {guess} es menor")
-    
-    if 
+
+
+    #IA del rival
+    computador = random.randint(1, 100)
+    if computador == nume_ran:
+        playing = False
+        print(f"el numero de tu rival: {computador} es el correcto")
+    elif computador > nume_ran:
+        print(f"el numero de tu rival: {computador} es mayor")
+    elif computador < nume_ran:
+        print(f"el numero de tu rival: {computador} es menor")
