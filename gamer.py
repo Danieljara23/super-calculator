6a9ff617e7ef7934873f4f75c74cfcbfa8cc572d
Amariles <alejoama2005@gmail.com>
2024-09-20 16:33:15 -0500

    coloreado

--- gamer.py
+++ gamer.py
@@ -1,28 +1,29 @@
 import random
+from rich import print as rprint
 
+#variables
 nume_ran = random.randint(1, 100)
 playing = True
 
-#el jugador
+#El jugador
 while playing:
     guess = int(input("Ingresa un numero:"))
     if guess == nume_ran:
         playing = False
-        print("¡¡Felicidades!! Eres el ganador")
+        rprint("[deep_pink4]¡¡Felicidades!! [medium_orchid3]Eres el ganador")
         break
     elif guess > nume_ran:
-        print(f"Tu numero: {guess} es mayor")
+        rprint(f"[green]Tu numero:[red]{guess} [yellow]es mayor") 
     elif guess < nume_ran:
-        print(f"Tu numero: {guess} es menor")
-
+        rprint(f"[green]Tu numero:[red]{guess} [cyan]es menor")
 
     #IA del rival
     computador = random.randint(1, 100)
     if computador == nume_ran:
         playing = False
-        print(f"Lo siento, pero el numero de tu rival: {computador} es el correcto")
+        rprint(f"[bright_red]Lo sentimos, pero el numero de tu rival: [green1]{computador} [bright_red]es el correcto")
         break
     elif computador > nume_ran:
-        print(f"El numero de tu rival: {computador} es mayor")
+        rprint(f"[dark_red]El numero de tu rival:[red]{computador} [yellow]es mayor")
     elif computador < nume_ran:
-        print(f"El numero de tu rival: {computador} es menor")
+        rprint(f"[dark_red]El numero de tu rival:[red]{computador} [cyan]es menor")
